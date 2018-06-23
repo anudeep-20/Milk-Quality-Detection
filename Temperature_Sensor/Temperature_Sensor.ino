090a508345b53ebc7140d630b49c382bf90b4350
Anudeep <anudeep.j06@gmail.com>
2018-06-23 22:01:10 +0530

    Add files via upload

---
+++ Temperature_Sensor/Temperature_Sensor.ino
@@ -0,0 +1,34 @@
+//Yellow Wire - A0
+//Black Wire - GND
+//Red Wire - 3.3V
+
+const int temperaturePin = A0;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  float voltage, degreesC, degreesF;
+  Serial.println(analogRead(temperaturePin));
+  voltage = analogRead(temperaturePin) * 0.004882814;
+  
+  degreesC = (voltage - 0.5) * 100.0;
+
+  degreesF = degreesC * (9.0/5.0) + 32.0;
+
+  // Now we'll use the serial port to print these values
+  
+  Serial.print("voltage: ");
+  Serial.print(voltage);
+  Serial.print("  deg C: ");
+  Serial.print(degreesC);
+  Serial.print("  deg F: ");
+  Serial.println(degreesF);
+
+  delay(1000); // repeat once per second (change as you wish!)
+}
+
